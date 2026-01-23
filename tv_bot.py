5390aa817662b2d9ebd8e6077575945351d5859b
Hariharan S <gopalhari1731@gmail.com>
2026-01-23 15:29:45 +0530

    Update tv_bot.py

--- tv_bot.py
+++ tv_bot.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -16,4 +17,6 @@ def webhook():
 
     return "OK", 200
 
-app.run(port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
